ab64ffd17454e2fe1d08da39283d18e9d81b1382
PriyankaVerma2307 <vermapriyanka32892@gmail.com>
2025-08-01 20:00:48 +0530

    setup

--- createUser.js
+++ createUser.js
@@ -10,7 +10,7 @@ mongoose.connect(process.env.MONGO_URI);
 
 (async () => {
   const hash = await bcrypt.hash('msg1234', 10);
-  await User.create({ username: 'msg', password: hash });
+  await User.create({ username: 'msgpapa', password: hash });
   console.log('✅ Admin user created');
   mongoose.disconnect();
 })();
